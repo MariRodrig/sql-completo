ca06b519dbc2b6aedaa78970afac87654cb4f164
Mariana Rodrigues <marianar.adm@outlook.com>
2026-02-15 13:50:48 -0300

    feat: Distinct

---
+++ aula02/distinct.sql
@@ -0,0 +1,12 @@
+
+
+-- DISTINCT: elimina valores repetidos de uma tabela
+
+SELECT DISTINCT FirstName
+FROM person.Person;
+
+
+-- DESAFIO 02: quantos sobrenomes unicos temos na tabela person.person?
+
+SELECT DISTINCT LastName
+FROM person.Person;
