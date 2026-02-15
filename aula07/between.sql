561cd798b27b8dbc56f5174a8eba720779a784bc
Mariana Rodrigues <marianar.adm@outlook.com>
2026-02-15 14:02:30 -0300

    feat: Between

---
+++ aula07/between.sql
@@ -0,0 +1,15 @@
+
+-- Between: usado para encontrar um valor, entre um valor minimo e um valor maximo
+
+SELECT * 
+FROM Production.Product
+WHERE ListPrice BETWEEN 1000 and 1500;
+
+SELECT * 
+FROM Production.Product
+WHERE ListPrice NOT BETWEEN 1000 and 1500;
+
+SELECT *
+FROM HumanResources.Employee
+WHERE HireDate BETWEEN '2009/01/01' and '2010/01/01'
+ORDER BY HireDate
