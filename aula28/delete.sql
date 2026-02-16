043823a2df73a218a0a9995f843b0d57bd682cec
Mariana Rodrigues <marianar.adm@outlook.com>
2026-02-16 13:47:23 -0300

    feat: Delete

---
+++ aula28/delete.sql
@@ -0,0 +1,11 @@
+
+-- DELETE
+-- remover registros da tabela
+-- importante: Se você não usar WHERE, apaga TODOS os registros da tabela
+
+
+SELECT *
+FROM aula
+
+DELETE FROM aula
+WHERE Nome = 'Aula 6';
