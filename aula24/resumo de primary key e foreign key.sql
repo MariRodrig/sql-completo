ce304c0ad93c2d75d70c94ad2f2e6c47462f21fd
Mariana Rodrigues <marianar.adm@outlook.com>
2026-02-16 13:45:36 -0300

    feat: Primary key e foreign key

---
+++ aula24/resumo de primary key e foreign key.sql
@@ -0,0 +1,7 @@
+
+-- CHAVE PRIMARIA (PRIMARY KEY) E CHAVE ESTRANGEIRA (FOREIGN KEY)
+
+-- PRIMARY KEY: e basicamente uma coluna ou grupo de colunas, usada para identificar unicamente uma linha em uma tabela
+
+-- FOREIGN KEY: e uma coluna ou grupo de colunas em uma tabela que identifica unicamente uma linha em outra tabela
+-- ou seja, a chave estrangeira e definida em uma tabela onde ela apenas faz referencia e nao contem todos os dados ali
