bb513e3bbaf90338e6355cde61257429df4ea585
Mariana Rodrigues <marianar.adm@outlook.com>
2026-02-16 13:48:19 -0300

    feat: Drop e truncate table

---
+++ aula30/drop e truncate table.sql
@@ -0,0 +1,13 @@
+
+-- DROP TABLE
+-- Apaga a tabela inteira do banco
+
+DROP TABLE ErrorLog;
+
+
+-- TRUNCATE TABLE
+-- Apaga todas as linhas da tabela
+SELECT *
+FROM Person.Password;
+
+TRUNCATE TABLE person.password;
