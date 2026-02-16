1467429b159e7bd89f5999ec69664188517f8ba4
Mariana Rodrigues <marianar.adm@outlook.com>
2026-02-16 13:41:11 -0300

    feat: Tipos de dados

---
+++ aula23/tipos de dados.sql
@@ -0,0 +1,23 @@
+
+-- TIPOS DE DADOS
+/*
+TEXTO
+CHAR/NCHAR: texto fixo, permite inserir ate uma quant fixa de caracteres, utilizando todo o espaco definido
+VARCHAR/NVARCHAR: texto variavel, permite inserir ate uma quant definida, porem so usa o espaco que foi preenchido
+
+NUMERO
+TINYINT: tipo numerico pequeno (0 a 255) e nao aceita valores negativos
+SMALLINT: maior que o tinyint (-32.768 a 32.767)
+INT: numeros inteiros
+BIGINT: inteiro grande
+DECIMAL: decimal preciso(p,s)
+FLOAT: decimal aproximado
+
+DATAS
+DATE: data 
+TIME: hora 
+DATETIME: data + hora
+
+BOOLEANOS
+BIT: por padrao e inicializado como nulo, podendo receber 0 ou 1 */
+
